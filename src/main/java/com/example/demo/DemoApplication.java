6e22193912e6cb872e7a98c1106f4f1e96859013
aydanrahimova <aydanragimova@gmail.com>
2025-05-28 08:09:55 +0400

    Added new message

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,9 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int a = 5;
+        System.out.println(a+4);
+        System.out.println("Smth new");
         SpringApplication.run(DemoApplication.class, args);
     }
 
